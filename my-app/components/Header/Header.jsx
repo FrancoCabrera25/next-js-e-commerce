12a296255d43b45b1ce1aa6f16b864db8270b4c8
Franco Cabrera <cabrera.franco@github.com>
2021-12-06 13:45:46 -0300

    add header and topBar

---
+++ my-app/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TopBar from './TopBar';
+
+const Header = () => {
+    return (
+        <div className="header">
+           <TopBar/>
+        </div>
+    );
+};
+
+export default Header;
--- my-app/components/layouts/Layout.jsx
+++ my-app/components/layouts/Layout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import  { Container } from 'semantic-ui-react';
+import  Header from '../Header';
 
 const Layout = (props) => {
     const { children } = props;
     return (
-        <Container fuild className="layout">
+        <Container fuild="true" className="layout">
+            <Header/>
             <Container className="content">
                 <h1>Layouts</h1>
                 {children}
